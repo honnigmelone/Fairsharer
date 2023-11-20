e66a8b1047749b1e21c4735ac2e8603adde61c6d
honnigmelone <nikita.rosik@study.hs-duesseldorf.de>
2023-11-20 05:58:23 +0100

    added fairsharer method

--- src/fairsharer.py
+++ src/fairsharer.py
@@ -1,16 +1,43 @@
 def fair_sharer(values, num_iterations, share=0.1):
-"""Runs num_iterations.
-In each iteration the highest value in values gives a fraction (share)
-to both the left and right neighbor. The leftmost field is considered
-the neightbor of the rightmost field.
-Examples:
-fair_sharer([0, 1000, 800, 0], 1) --> [100, 800, 900, 0]
-fair_sharer([0, 1000, 800, 0], 2) --> [100, 890, 720, 90]
-Args
-values:
-1D array of values (list or numpy array)
-num_iteration:
-Integer to set the number of iterations
-"""
-# code
-  return values_new
+    """
+    Runs num_iterations.
+    In each iteration the highest value in values gives a fraction (share)
+    to both the left and right neighbor. The leftmost field is considered
+    the neighbor of the rightmost field.
+    Examples:
+    fair_sharer([0, 1000, 800, 0], 1) --> [100, 800, 900, 0]
+    fair_sharer([0, 1000, 800, 0], 2) --> [100, 890, 720, 90]
+    Args
+    values:
+    1D array of values (list or numpy array)
+    num_iteration:
+    Integer to set the number of iterations
+    """
+    for _ in range(num_iterations):
+        # get index of highest value and the share
+        highest_value =  max(values)
+        value_share = highest_value * share
+        index = values.index(highest_value)
+
+        values[index] -= value_share * 2
+
+        # calculate new values and save as values_new
+        if index != 0 and index != len(values):
+          values[index - 1] += value_share
+          values[index + 1] += value_share
+
+        # get circular shares for left and rightmost highest number
+        if index == 0:
+           values[len(values) - 1] += value_share
+           values[index + 1] += value_share
+        
+        if index == len(values):
+           values[0] += value_share
+           values[index - 1] += value_share
+
+    values_new = values
+    return values_new
+    
+    
+print(fair_sharer([1000, 0, 800, 0], 3))
+
